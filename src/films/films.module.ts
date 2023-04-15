5867a410adacfb2c7e162ac8fff03d2e29fae706
Andres Lobaton <andrespipe021028@gmail.com>
2023-04-15 00:35:06 -0500

    feat: set docker file

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FilmsService } from './services/films.service';
 import { FilmsController } from './controllers/films.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { GendersController } from './controllers/genders.controller';
+import { GendersService } from './services/genders.service';
+import { CategoriesService } from './services/categories.service';
 
 @Module({
-  controllers: [FilmsController],
-  providers: [FilmsService],
+  controllers: [FilmsController, CategoriesController, GendersController],
+  providers: [FilmsService, GendersService, CategoriesService],
 })
 export class FilmsModule {}
